551da8a7cf114b9d7341b46ceb95fb86f1f47cbc
Daryl <darylcecile@gmail.com>
2019-12-17 17:45:43 +0000

    Added Less
    - Added build script for compiling less
    - Added less support
    - Moved meta for index.ejs page to meta.ejs

---
+++ build.js
@@ -0,0 +1,35 @@
+
+
+const lessc = require("less");
+const fs = require("fs");
+const path = require("path");
+
+const STYLES_FOLDER = path.resolve("./public/styles");
+
+let entries = fs.readdirSync( STYLES_FOLDER );
+
+entries.forEach(entry => {
+
+    let entryPath = path.join(STYLES_FOLDER,entry);
+    let outputPath = path.join(STYLES_FOLDER, entry.replace(".less",".css") );
+    let sourceMapPath = path.join(STYLES_FOLDER, entry.replace(".less", ".css.map"));
+
+    if ( fs.statSync( entryPath ).isFile() ){
+
+        if ( entry.startsWith("__") === false && entry.endsWith(".less") ){
+
+            lessc.render( fs.readFileSync(entryPath, {encoding:"utf8"}) , {
+                env: "development",
+                sourceMap: { sourceMapFileInline: false }
+            }).then(function(out){
+                fs.writeFileSync(outputPath,out.css,{encoding:"utf8"});
+                fs.writeFileSync(sourceMapPath,out.map,{encoding:"utf8"});
+            }, function(err){
+                throw err;
+            });
+
+        }
+
+    }
+
+});
