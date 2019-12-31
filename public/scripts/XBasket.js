4e9c0baeb8d6f9825aa9c0ded1c5b966ca1453d0
Riz <rizakmalkhan@gmail.com>
2019-12-31 18:36:03 +0000

    added product list

--- public/scripts/XBasket.js
+++ public/scripts/XBasket.js
@@ -268,4 +268,127 @@ class XBasket extends HTMLElement {
     }
 }
 customElements.define('x-basket', XBasket);
+class XProduct extends HTMLElement {
+    constructor() {
+        super();
+        this._collection = null;
+        this._shadow = this.attachShadow({ mode: "open" });
+    }
+    get collection() {
+        return this._collection;
+    }
+    connectedCallback() {
+        this.setup();
+    }
+    disconnectedCallback() {
+        this.reset();
+    }
+    attachCollection(collection) {
+        this.detachCollection();
+        this._collection = collection;
+        this._collection.onUpdate(() => this.updateVisuals());
+        this.refresh();
+        return this;
+    }
+    detachCollection() {
+        if (this._collection === null)
+            return this;
+        this._collection.onUpdate(() => { });
+        this._collection = null;
+        return this;
+    }
+    updateVisuals() {
+        this._shadow.querySelector("#title").innerHTML = `<strong> Product </strong> <span>(${this.itemCount} Items)</span>`;
+        this._shadow.querySelector(".checkout-btn").innerHTML = `<button>Checkout ${this.itemCount} item(s)</span>`;
+        let currentItemElementIds = [...this._shadow.querySelectorAll("ul.b-list > li")].map(el => el.id.replace("item-", ""));
+        if (this.itemCount > 0) {
+            this.collection.eachItem((item, prev, next) => {
+                let existing = this._shadow.querySelector("#item-" + item.itemReference);
+                currentItemElementIds.splice(currentItemElementIds.indexOf(item.itemReference), 1);
+                if (!existing) {
+                    let container = this._shadow.querySelector("ul.b-list");
+                    let it = this.createItemElement(item);
+                    if (next) {
+                        let nextEl = this._shadow.querySelector("#item-" + next.itemReference);
+                        if (nextEl)
+                            nextEl.insertAdjacentElement("beforebegin", it);
+                        else
+                            container.appendChild(it);
+                    }
+                    else if (prev) {
+                        let prevEl = this._shadow.querySelector("#item-" + prev.itemReference);
+                        if (prevEl)
+                            prevEl.insertAdjacentElement("afterend", it);
+                        else
+                            container.appendChild(it);
+                    }
+                    else {
+                        container.appendChild(it);
+                    }
+                }
+                else {
+                    existing.querySelector("img").src = item.imageUrl;
+                    existing.querySelector("p:nth-of-type(1)").innerHTML = item.name;
+                    existing.querySelector("p:nth-of-type(2)")
+                        .querySelector("span:nth-of-type(1)").innerHTML = `Quantity: ${item.quantity}`;
+                }
+            });
+            currentItemElementIds.forEach(itemId => {
+                let itemEl = this._shadow.querySelector("#item-" + itemId);
+                itemEl.classList.add("hiding");
+                setTimeout(() => {
+                    itemEl.remove();
+                }, 240);
+            });
+        }
+    }
+    setup() {
+        let l = document.createElement("link");
+        l.href = "/public/styles/xbasket.css";
+        l.rel = "stylesheet";
+        this._shadow.appendChild(l);
+        let s = document.createElement("link");
+        s.href = "https://kit-free.fontawesome.com/releases/latest/css/free.min.css";
+        s.rel = "stylesheet";
+        this._shadow.appendChild(s);
+        let container = document.createElement("ul");
+        container.className = "b-list";
+        this._shadow.appendChild(container);
+        if (this.itemCount > 0) {
+            this.collection.eachItem(item => {
+                let it = this.createItemElement(item);
+                container.appendChild(it);
+            });
+        }
+    }
+    reset() {
+        if (this._shadow !== null)
+            this._shadow.innerHTML = "";
+    }
+    refresh() {
+        this.reset();
+        this.setup();
+        return this;
+    }
+    get itemCount() {
+        if (this._collection) {
+            return this._collection.itemCount;
+        }
+        return 0;
+    }
+    createItemElement(item) {
+        let it = document.createElement("li");
+        it.id = "item-" + item.itemReference;
+        let img = document.createElement("img");
+        img.src = item.imageUrl;
+        it.appendChild(img);
+        let div = document.createElement("div");
+        it.appendChild(div);
+        let nameEl = document.createElement("p");
+        nameEl.innerHTML = item.name;
+        div.appendChild(nameEl);
+        return it;
+    }
+}
+customElements.define('x-product', XProduct);
 //# sourceMappingURL=XBasket.js.map
