74e2f6c1e4b5e8374b9fa9662c7ed86a13c80db9
Daryl <darylcecile@gmail.com>
2020-01-17 14:35:44 +0000

    lose build crap

--- build.js
+++ build.js
@@ -4,23 +4,68 @@ const lessc = require("less");
 const fs = require("fs");
 const path = require("path");
 
-function clearBuiltFiles(){
+const FORCE_DELETE_BUILT_ITEM = true;
+
+function fixGitIgnore(filesToIgnore){
+    let fileContent = fs.readFileSync( path.join(__dirname,".gitignore") , 'utf-8');
+    let lines = fileContent.split("\n");
+    let overwrite = false;
+
+    let newFileLines = [];
+
+    lines.forEach(line => {
+        if (line.trim() === "### BEGIN clean"){
+            newFileLines.push(line);
+            overwrite = true;
+            return;
+        }
+        else if (line.trim() === "### END"){
+            newFileLines.push(line);
+            overwrite = false;
+            return;
+        }
+        else if (overwrite === true){
+            newFileLines.push( ...(filesToIgnore.map(k => k.replace(`${__dirname}/`,""))) );
+            overwrite = false;
+        }
+        else{
+            newFileLines.push(line);
+        }
+    });
+
+    fs.writeFileSync( path.join(__dirname,".gitignore"), newFileLines.join("\n") , 'utf-8');
+
+    console.log("Updated gitignore");
+}
+
+function ignoreBuiltFiles(ref) {
+
+    let toIgnore = [];
 
     function rm(thePath) {
         let removedItems = [];
-        let fileNameNoExt = path.basename(thePath,".js.map");
-        let srcFile = path.join(path.dirname( thePath ) , fileNameNoExt + ".js");
-        if ( fs.existsSync( srcFile) ) {
-            fs.unlinkSync( srcFile );
-            removedItems.push(srcFile);
+
+        Object.keys(ref).map(coreExt => {
+            ref[coreExt].forEach(k => {
+                let p = path.join( path.dirname(thePath) , path.basename(thePath,`.${coreExt}`) + k);
+                removedItems.push(p);
+            });
+        });
+
+        if (FORCE_DELETE_BUILT_ITEM){
+            removedItems.forEach(item => {
+                if (fs.existsSync(item)){
+                    fs.unlinkSync(item);
+                }
+            })
         }
-        fs.unlinkSync(thePath);
-        removedItems.push(thePath);
-        console.log("Removed", removedItems.join("and") );
+
+        if (removedItems.length > 0){
+            toIgnore = [...toIgnore, ...removedItems];
+        }
     }
 
     function walk(thePath){
-        console.log("Walking",thePath);
         let filesInRoot = fs.readdirSync(thePath);
 
         filesInRoot.forEach(item => {
@@ -28,23 +73,33 @@ function clearBuiltFiles(){
             if ( path.basename(item) === "node_modules" ) return;
             if ( path.basename(item) === ".github" ) return;
             if ( path.basename(item) === ".vscode" ) return;
+            if ( path.basename(item) === ".git" ) return;
 
             let stat = fs.statSync(item);
             if (stat.isDirectory()) return walk(item);
 
-            if ( item.endsWith(".js.map") ) rm( item );
+            Object.keys(ref).forEach(k => {
+                if ( item.endsWith(k) ) rm( item );
+            });
 
         });
     }
 
     walk(__dirname);
 
+    fixGitIgnore(toIgnore)
 }
 
-function buildLess(){
+function buildLess(cb){
+
+    function decrementTaskCount(){
+        tasksRemaining --;
+        if (tasksRemaining === 0 && cb) cb();
+    }
 
     const STYLES_FOLDER = path.resolve("./public/styles");
 
+    let tasksRemaining = 0;
     let entries = fs.readdirSync( STYLES_FOLDER );
 
     entries.forEach(entry => {
@@ -57,6 +112,8 @@ function buildLess(){
 
             if ( entry.startsWith("__") === false && entry.endsWith(".less") ){
 
+                tasksRemaining ++;
+
                 lessc.render( fs.readFileSync(entryPath, {encoding:"utf8"}) , {
                     env: "development",
                     sourceMap: { sourceMapFileInline: false },
@@ -64,8 +121,10 @@ function buildLess(){
                 }).then(function(out){
                     fs.writeFileSync(outputPath,out.css,{encoding:"utf8"});
                     fs.writeFileSync(sourceMapPath,out.map,{encoding:"utf8"});
+                    decrementTaskCount();
                 }).catch(function(err){
                     console.error(err);
+                    decrementTaskCount();
                 });
 
             }
@@ -76,6 +135,13 @@ function buildLess(){
 
 }
 
-clearBuiltFiles();
+buildLess(()=>{
 
-buildLess();
+    ignoreBuiltFiles({
+        ts:[".js.map",".d.ts"],
+        less:[".css",".css.map"]
+    });
+
+});
+
+
