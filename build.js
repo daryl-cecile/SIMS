7c70da7c2dd33fa77e8f39001490702b08ee29b2
Daryl <darylcecile@gmail.com>
2020-01-17 12:46:22 +0000

    Update build.js

--- build.js
+++ build.js
@@ -4,33 +4,78 @@ const lessc = require("less");
 const fs = require("fs");
 const path = require("path");
 
-const STYLES_FOLDER = path.resolve("./public/styles");
+function clearBuiltFiles(){
 
-let entries = fs.readdirSync( STYLES_FOLDER );
+    function rm(thePath) {
+        let removedItems = [];
+        let fileNameNoExt = path.basename(thePath,".js.map");
+        let srcFile = path.join(path.dirname( thePath ) , fileNameNoExt + ".js");
+        if ( fs.existsSync( srcFile) ) {
+            fs.unlinkSync( srcFile );
+            removedItems.push(srcFile);
+        }
+        fs.unlinkSync(thePath);
+        removedItems.push(thePath);
+        console.log("Removed", removedItems.join("and") );
+    }
 
-entries.forEach(entry => {
+    function walk(thePath){
+        console.log("Walking",thePath);
+        let filesInRoot = fs.readdirSync(thePath);
 
-    let entryPath = path.join(STYLES_FOLDER,entry);
-    let outputPath = path.join(STYLES_FOLDER, entry.replace(".less",".css") );
-    let sourceMapPath = path.join(STYLES_FOLDER, entry.replace(".less", ".css.map"));
+        filesInRoot.forEach(item => {
+            item = path.join(thePath, item);
+            if ( path.basename(item) === "node_modules" ) return;
+            if ( path.basename(item) === ".github" ) return;
+            if ( path.basename(item) === ".vscode" ) return;
 
-    if ( fs.statSync( entryPath ).isFile() ){
+            let stat = fs.statSync(item);
+            if (stat.isDirectory()) return walk(item);
 
-        if ( entry.startsWith("__") === false && entry.endsWith(".less") ){
+            if ( item.endsWith(".js.map") ) rm( item );
 
-            lessc.render( fs.readFileSync(entryPath, {encoding:"utf8"}) , {
-                env: "development",
-                sourceMap: { sourceMapFileInline: false },
-                paths: [STYLES_FOLDER]
-            }).then(function(out){
-                fs.writeFileSync(outputPath,out.css,{encoding:"utf8"});
-                fs.writeFileSync(sourceMapPath,out.map,{encoding:"utf8"});
-            }).catch(function(err){
-                console.error(err);
-            });
+        });
+    }
+
+    walk(__dirname);
+
+}
+
+function buildLess(){
+
+    const STYLES_FOLDER = path.resolve("./public/styles");
+
+    let entries = fs.readdirSync( STYLES_FOLDER );
+
+    entries.forEach(entry => {
+
+        let entryPath = path.join(STYLES_FOLDER,entry);
+        let outputPath = path.join(STYLES_FOLDER, entry.replace(".less",".css") );
+        let sourceMapPath = path.join(STYLES_FOLDER, entry.replace(".less", ".css.map"));
+
+        if ( fs.statSync( entryPath ).isFile() ){
+
+            if ( entry.startsWith("__") === false && entry.endsWith(".less") ){
+
+                lessc.render( fs.readFileSync(entryPath, {encoding:"utf8"}) , {
+                    env: "development",
+                    sourceMap: { sourceMapFileInline: false },
+                    paths: [STYLES_FOLDER]
+                }).then(function(out){
+                    fs.writeFileSync(outputPath,out.css,{encoding:"utf8"});
+                    fs.writeFileSync(sourceMapPath,out.map,{encoding:"utf8"});
+                }).catch(function(err){
+                    console.error(err);
+                });
+
+            }
 
         }
 
-    }
+    });
 
-});
+}
+
+clearBuiltFiles();
+
+buildLess();
