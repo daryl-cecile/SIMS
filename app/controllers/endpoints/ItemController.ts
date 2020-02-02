9ea253e807b4a1b0189cbc647f61ef406592dc8a
SeanSkid <seanskid09@gmail.com>
2020-02-02 16:49:02 +0000

    Update ItemController.ts
    Added ItemList

--- app/controllers/endpoints/ItemController.ts
+++ app/controllers/endpoints/ItemController.ts
@@ -5,13 +5,13 @@ import {ItemRepository} from "../../Repository/ItemRepository";
 export const ItemManagementController = new RouterSet((router) => {
 
     router.get("/items/search", async function(req, res) {
-        let searchResults = await ItemRepository.findByText(req.body["term"]);
+        let searchResults = await ItemRepository.getByItemCode(req.body["itemId"]);
         res.json(JSONResponse(true, "Results", searchResults))
     });
 
-    router.post("/items/item", async function (req, res){
-        // ItemModel
-        res.json(JSONResponse(true, "ItemDetails"));
+    router.post("/items/itemlist", async function (req, res){
+        let listResults = await ItemRepository.getAll();
+        res.json(JSONResponse(true, "ItemList", listResults));
     });
 
     return router;
