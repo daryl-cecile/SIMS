1c0dfca17494445433c02edd1c9585638b6489fc
Alex McBean <al.mcbean@outlook.com>
2020-02-02 14:30:14 +0000

    Item Searching
    /items/search now returns list of items matching the given term
    
    Signed-off-by: Alex McBean <al.mcbean@outlook.com>

--- app/controllers/endpoints/ItemController.ts
+++ app/controllers/endpoints/ItemController.ts
@@ -1,23 +1,18 @@
 import {RouterSet} from "../../config/RouterSet";
 import {JSONResponse} from "../../config/JSONResponse";
+import {ItemRepository} from "../../Repository/ItemRepository";
 
 export const ItemManagementController = new RouterSet((router) => {
 
     router.get("/items/search", async function(req, res) {
-        res.json
-
+        let searchResults = await ItemRepository.findByText(req.body["term"]);
+        res.json(JSONResponse(true, "Results", searchResults))
     });
 
-    router.post("/item-management/item", async function (req, res){
+    router.post("/items/item", async function (req, res){
         // ItemModel
         res.json(JSONResponse(true, "ItemDetails"));
     });
 
-    router.post("/item-management/itemlist", async function (req, res){
-        // ItemList
-        res.json(JSONResponse(true, "ItemListDetails"));
-    });
-
     return router;
-
 });
