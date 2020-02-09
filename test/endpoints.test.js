68c70b5f42947b8a9104865aec92227240371817
Alex McBean <al.mcbean@outlook.com>
2020-02-09 21:43:56 +0000

    Building test units for backend
    Signed-off-by: Alex McBean <al.mcbean@outlook.com>

---
+++ test/endpoints.test.js
@@ -0,0 +1,29 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let bootstrapper = require('../index');
+
+
+
+bootstrapper.enableTestMode();
+
+chai.use(chaiHttp);
+
+describe('Transactions Check', ()=>{
+
+});
+
+describe('Inventory Checks', ()=>{
+
+});
+
+describe('Item Checks', ()=>{
+
+});
+
+describe('Login Check', ()=>{
+
+});
+
+describe('User Checks', ()=>{
+
+});
