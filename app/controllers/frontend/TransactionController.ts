5289416a8a068961e147966cb310b5076c7d6e7e
Aurimas Gikys <aurimas.gikys@gmail.com>
2020-02-13 16:24:29 +0000

    Update TransactionController.ts

--- app/controllers/frontend/TransactionController.ts
+++ app/controllers/frontend/TransactionController.ts
@@ -6,10 +6,14 @@ export const TransactionController = new RouterSet( (router) => {
         res.render("pages/confirm_transaction");
     });
 
-    router.get("/help", async function(req, res){
-        res.render("pages/help");
     router.get("/report", async function(req, res){
         res.render("partials/report");
     });
+
+    router.get("/help", async function(req, res){
+        res.render("pages/help");
+    })
+
     return router;
+
 });
